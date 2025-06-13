959e70590d5bbaa0ee886551df1cd787943418c1
Shivesh Chaudhary <126916276+shiveshc710@users.noreply.github.com>
2025-06-12 22:24:53 -0400

    Update HeaderMenu.jsx

--- src/components/HeaderMenu/HeaderMenu.jsx
+++ src/components/HeaderMenu/HeaderMenu.jsx
@@ -1,8 +1,25 @@
 import React, { useState } from "react";
 import "./HeaderMenu.css";
+import { useDispatch, useSelector } from "react-redux";
+import { changeMenu } from "../../redux/slices/itemSlice";
 
 export default function HeaderMenu() {
   const [menu, setMenu] = useState(["Home", "Product List", "Shopping Cart"]);
+  const totalItems = useSelector((state) => state.cart.totalItems);
+  const dispatch = useDispatch();
+
+  const handleClick = (index) => {
+    let currSelection = "";
+    if (index === 0) {
+      currSelection = "home";
+    } else if (index === 1) {
+      currSelection = "products";
+    } else if (index === 2) {
+      currSelection = "cart";
+    }
+
+    dispatch(changeMenu(currSelection));
+  };
 
   return (
     <>
@@ -12,7 +29,11 @@ export default function HeaderMenu() {
           if (idx < menu.length - 1) {
             return (
               <>
-                <span className="menu-item" key={idx}>
+                <span
+                  className="menu-item"
+                  key={idx}
+                  onClick={() => handleClick(idx)}
+                >
                   {item}
                 </span>
                 <span style={{ marginLeft: "20px" }}> | </span>
@@ -20,8 +41,12 @@ export default function HeaderMenu() {
             );
           }
           return (
-            <span className="menu-item" key={idx}>
-              {item}
+            <span
+              className="menu-item"
+              key={idx}
+              onClick={() => handleClick(idx)}
+            >
+              {item} [{totalItems}]
             </span>
           );
         })}
