d45996f136801ab30eae90b6c03b6c140e9dbdcc
Shivesh Chaudhary <126916276+shiveshc710@users.noreply.github.com>
2025-06-13 00:38:24 -0400

    Final Fix

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import logo from "./logo.svg";
-import "./App.css";
+// import "./App.css";
 import LandingPage from "./components/LandingPage/LandingPage";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 function App() {
-  return <LandingPage />;
+  return (
+    <Provider store={store}>
+      <LandingPage />
+    </Provider>
+  );
 }
 
 export default App;
